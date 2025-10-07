eef4a82f02b5d55329e970a73e8cc9724fe7b5ae
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-07 22:51:40 +0700

    Auto Increment

---
+++ src/test/java/indra/dwi/database/AutoIncrementTest.java
@@ -0,0 +1,30 @@
+package indra.dwi.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class AutoIncrementTest {
+
+    @Test
+    void testAutoIncrement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        String sql = "INSERT INTO comments(email, comment) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setString(1, "indradwi@test");
+        preparedStatement.setString(2, "hihi");
+
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        if (resultSet.next()) {
+            System.out.println("Id comment: " + resultSet.getInt(1));
+        }
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+}
